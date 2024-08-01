f65320ff11059392b8e9bb39c29087b612c4ee77
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-01 14:31:42 +0300

    add: state hook to formAddFriend

--- src/App.js
+++ src/App.js
@@ -87,13 +87,43 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImageLink] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const new_friend = {
+      id,
+      name,
+      image: `${image}?u=${id}`,
+      balance: 0,
+    };
+
+    console.log(new_friend);
+
+    setName("");
+    setImageLink("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👨🏽‍🤝‍👨🏽Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🖼 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImageLink(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
